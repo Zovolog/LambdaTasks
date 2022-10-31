52c71e160dc94ef8722d5b7631c9f8d0c67c8323
@Zovolog <zimoror@gmail.com>
2022-10-31 13:11:01 +0100

    Token was hidden

--- 4. CLI Telegram Console Sender/script.js
+++ 4. CLI Telegram Console Sender/script.js
@@ -2,7 +2,7 @@ const program = require("commander");
 
 const TelegramBot = require("node-telegram-bot-api");
 
-const token = "5766619151:AAGKA9uJTJUFCYPjUOG4hGTap5rWIHvQVpU";
+const token = "token";
 const bot = new TelegramBot(token, { polling: false });
 
 program
@@ -11,7 +11,7 @@ program
   .argument("<message>")
   .alias("m")
   .action(function (name) {
-    bot.sendMessage(609114632, name);
+    bot.sendMessage(myId, name);
   });
 
 program
@@ -20,7 +20,7 @@ program
   .argument("<path>")
   .alias("p")
   .action(function (name) {
-    bot.sendPhoto(609114632, name);
+    bot.sendPhoto(myId, name);
   });
 
 program.parse(process.argv);
